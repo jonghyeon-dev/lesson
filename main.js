f1a032779a9e9deda907a7e3982d2b4de317f141
jhPark <jonghyeon0780@gmail.com>
2023-12-04 13:31:53 +0900

    충돌

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let userName = "Taken John";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 let a = "ccccc";
+=======
+function error(userName){
+    console.log(userName);
+};
+>>>>>>> Stashed changes
 =======
 function error(userName){
     console.log(userName);
